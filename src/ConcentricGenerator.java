5158d7424a1e472c53a2d7102987530f8f0a78ff
XoulKool <tuf43782@temple.edu>
2017-03-13 20:53:43 -0400

    2/22/2017 10:37 AM

---
+++ src/ConcentricGenerator.java
@@ -0,0 +1,87 @@
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
+public class ConcentricGenerator extends Group {
+
+     private class Ripple extends Circle {
+
+     Timeline animation = new Timeline(
+     new KeyFrame(Duration.ZERO, new KeyValue(radiusProperty(), 0)),
+     new KeyFrame(Duration.seconds(1), new KeyValue(opacityProperty(), 100)),
+     new KeyFrame(Duration.seconds(10), new KeyValue(radiusProperty(), 1000)),
+     new KeyFrame(Duration.seconds(10), new KeyValue(opacityProperty(), 0))
+     );
+     
+     
+     
+
+     private Ripple(double centerX, double centerY) {
+     super(centerX, centerY, 0, null);
+     setStroke(Color.web("white", 0.16));
+     setStrokeWidth(3);
+     //animation.setRate(.2);
+     }
+     }
+
+     private double generatorCenterX = 100.0;
+     private double generatorCenterY = 100.0;
+
+     private Timeline generate = new Timeline(
+     new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+     @Override
+     public void handle(ActionEvent event) {
+     createRipple();
+     }
+     }
+     )
+     );
+
+     public ConcentricGenerator() {
+     generate.setCycleCount(Timeline.INDEFINITE);
+     this.createRipple();
+     generate.setRate(6);
+     this.startGenerating();
+     }
+
+     public void createRipple() {
+     final Ripple ripple = new Ripple(generatorCenterX, generatorCenterY);
+     getChildren().add(ripple);
+     ripple.animation.play();
+
+     Timeline remover = new Timeline(
+     new KeyFrame(Duration.seconds(10), new EventHandler<ActionEvent>() {
+     @Override
+     public void handle(ActionEvent event) {
+     getChildren().remove(ripple);
+     ripple.animation.stop();
+     }
+     })
+     );
+     remover.play();
+     }
+
+     public void startGenerating() {
+     generate.play();
+     }
+
+     public void stopGenerating() {
+     generate.stop();
+     }
+
+     public void setGeneratorCenterX(double generatorCenterX) {
+     this.generatorCenterX = generatorCenterX;
+     }
+
+     public void setGeneratorCenterY(double generatorCenterY) {
+     this.generatorCenterY = generatorCenterY;
+     }
+
+     }
--- src/XKVisualUI.java
+++ src/XKVisualUI.java
@@ -8,8 +8,10 @@ import java.io.File;
 import static java.lang.Math.floor;
 import static java.lang.Math.random;
 import static java.lang.String.format;
+import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
+import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
 
 import javafx.application.Application;
@@ -32,15 +34,23 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
+import static javafx.scene.paint.Color.color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
+import javafx.scene.shape.ArcTo;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.ClosePath;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.PathBuilder;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 import javafx.stage.FileChooser;
@@ -64,7 +74,7 @@ public class XKVisualUI extends Application {
     double height;
     MediaView mediaView;
     Group root;
-    
+
     @Override
     public void start(Stage primaryStage) {
 
@@ -82,41 +92,26 @@ public class XKVisualUI extends Application {
         BorderPane borderPane = new BorderPane();
 
         root = new Group();
-        animation(root);
-        borderPane.setCenter(root);
-        borderPane.setBottom(addToolBar());
+        Rectangle rect = new Rectangle(1000, 600);
+        rect.setFill(Color.BLUE);
+
+        ConcentricGenerator conCircles = new ConcentricGenerator();
+        //animation(conCircles);
         
-        System.out.println(borderPane.getCenter().getBoundsInParent());
+        circlePath(conCircles);
+        blendWithGrad(conCircles);
+
+        Pane pane = new Pane();
+        pane.getChildren().addAll(root);
+
+        borderPane.setCenter(pane);
+        borderPane.setBottom(addToolBar());
 
         borderPane.setStyle("-fx-background-color: Black");
 
         scene = new Scene(borderPane, 1000, 800);
         scene.setFill(Color.BLACK);
 
-        /*scene.setOnMousePressed(new EventHandler<MouseEvent>() {
-         @Override
-         public void handle(MouseEvent event) {
-         rippler.setGeneratorCenterX(event.getSceneX());
-         rippler.setGeneratorCenterY(event.getSceneY());
-         rippler.createRipple();
-         rippler.startGenerating();
-         }
-         });
-
-         scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
-         @Override
-         public void handle(MouseEvent event) {
-         rippler.setGeneratorCenterX(event.getSceneX());
-         rippler.setGeneratorCenterY(event.getSceneY());
-         }
-         });
-
-         scene.setOnMouseReleased(new EventHandler<MouseEvent>() {
-         @Override
-         public void handle(MouseEvent event) {
-         rippler.stopGenerating();
-         }
-         });*/
         stage.setTitle("XKVisual");
         stage.setScene(scene);
         fullScreenButton.setOnAction((ActionEvent e) -> {
@@ -126,16 +121,7 @@ public class XKVisualUI extends Application {
                 stage.setFullScreen(true);
             }
         });
-        
         stage.show();
-
-        DropShadow dropshadow = new DropShadow();
-        dropshadow.setOffsetY(5.0);
-        dropshadow.setOffsetX(5.0);
-        dropshadow.setColor(Color.WHITE);
-
-        mediaView.setEffect(dropshadow);
-
         fileOpenButton.setOnAction((ActionEvent e) -> {
             mediaPlayer.stop();
             restart(stage);
@@ -286,91 +272,87 @@ public class XKVisualUI extends Application {
         }
     }
 
-    /*class RippleGenerator extends Group {
-
-     private class Ripple extends Circle {
-
-     Timeline animation = new Timeline(
-     new KeyFrame(Duration.ZERO, new KeyValue(radiusProperty(), 0)),
-     new KeyFrame(Duration.seconds(1), new KeyValue(opacityProperty(), 1)),
-     new KeyFrame(Duration.seconds(1), new KeyValue(radiusProperty(), 100)),
-     new KeyFrame(Duration.seconds(1), new KeyValue(opacityProperty(), 0))
-     );
-
-     private Ripple(double centerX, double centerY) {
-     super(centerX, centerY, 0, null);
-     setStroke(Color.rgb(200, 200, 255));
-     }
-     }
-
-     private double generatorCenterX = 100.0;
-     private double generatorCenterY = 100.0;
-
-     private Timeline generate = new Timeline(
-     new KeyFrame(Duration.seconds(.25), new EventHandler<ActionEvent>() {
-     @Override
-     public void handle(ActionEvent event) {
-     createRipple();
-     }
-     }
-     )
-     );
-
-     public RippleGenerator() {
-     generate.setCycleCount(Timeline.INDEFINITE);
-     this.createRipple();
-     generate.setRate(0.5);
-     this.startGenerating();
-     
-     }
-
-     public void createRipple() {
-     final Ripple ripple = new Ripple(generatorCenterX, generatorCenterY);
-     getChildren().add(ripple);
-     ripple.animation.play();
-
-     Timeline remover = new Timeline(
-     new KeyFrame(Duration.seconds(10), new EventHandler<ActionEvent>() {
-     @Override
-     public void handle(ActionEvent event) {
-     getChildren().remove(ripple);
-     ripple.animation.stop();
-     }
-     })
-     );
-     remover.play();
-     }
-
-     public void startGenerating() {
-     generate.play();
-     }
-
-     public void stopGenerating() {
-     generate.stop();
-     }
-
-     public void setGeneratorCenterX(double generatorCenterX) {
-     this.generatorCenterX = generatorCenterX;
-     }
-
-     public void setGeneratorCenterY(double generatorCenterY) {
-     this.generatorCenterY = generatorCenterY;
-     }
-
-     }*/
-       
-    
     public void animation(Group group) {
 
         Group circles = new Group();
-        for (int i = 0; i < 10; i++) {
-            Circle circle = new Circle(50, Color.web("white", 0.05));
+        for (int i = 0; i < 100; i++) {
+            Circle circle = new Circle(25, Color.web("white", 0.05));
             circle.setStrokeType(StrokeType.OUTSIDE);
             circle.setStroke(Color.web("white", 0.16));
             circle.setStrokeWidth(4);
+            circle.setCenterX(500);
+            circle.setCenterY(300);
             circles.getChildren().add(circle);
         }
 
+
+        /*root.getChildren().add(colors);
+         root.getChildren().add(circles);*/
+
+        Timeline timeline = new Timeline();
+         for (Node circle : circles.getChildren()) {
+         timeline.getKeyFrames().addAll(
+         new KeyFrame(Duration.ZERO, // set start position at 0
+         new KeyValue(circle.translateXProperty(), 0),
+         new KeyValue(circle.translateYProperty(), 0)
+         ),
+         new KeyFrame(new Duration(2000), // set end position at 40s
+         new KeyValue(circle.translateXProperty(), random() * 1000),
+         new KeyValue(circle.translateYProperty(), random() * 1000)
+         )
+         );
+         }
+        /*EventHandler onFinished = new EventHandler<ActionEvent>() {
+         public void handle(ActionEvent t) {
+         group.getChildren().remove(blendModeGroup);
+         animation(group);
+         }
+         };*/
+        //KeyFrame keyFrame = new KeyFrame(new Duration(4000), onFinished);
+        //add the keyframe to the timeline
+        //timeline.getKeyFrames().add(keyFrame);
+        // play 40s of animation
+        //timeline.play();
+    }
+
+    public void circlePath(Node node) {
+
+        Path path = createEllipsePath(1400, 400, 700, 300, 0);
+
+        //path.getElements().add(new MoveTo(50, 50));
+        //path.getElements().add(arcTo);
+        PathTransition pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.millis(4000));
+        pathTransition.setPath(path);
+        pathTransition.setNode(node);
+        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTransition.setCycleCount(Timeline.INDEFINITE);
+        pathTransition.setInterpolator(Interpolator.LINEAR);
+        pathTransition.setAutoReverse(false);
+        pathTransition.setRate(.5);
+        pathTransition.play();
+    }
+
+    private Path createEllipsePath(double centerX, double centerY, double radiusX, double radiusY, double rotate) {
+        ArcTo arcTo = new ArcTo();
+        arcTo.setX(centerX - radiusX + 1); // to simulate a full 360 degree celcius circle.
+        arcTo.setY(centerY - radiusY);
+        arcTo.setSweepFlag(false);
+        arcTo.setLargeArcFlag(true);
+        arcTo.setRadiusX(radiusX);
+        arcTo.setRadiusY(radiusY);
+        arcTo.setXAxisRotation(rotate);
+
+        Path path = PathBuilder.create()
+                .elements(
+                        new MoveTo(centerX - radiusX, centerY - radiusY),
+                        arcTo,
+                        new ClosePath()) // close 1 px gap.
+                .build();
+        return path;
+    }
+
+    public void blendWithGrad(Group group) {
         LinearGradient grad = new LinearGradient(0f, 1f, 1f, 0f, true, CycleMethod.NO_CYCLE, new Stop[]{
             new Stop(0, Color.web("green")),
             new Stop(0.14, Color.web("turquoise")),
@@ -379,48 +361,14 @@ public class XKVisualUI extends Application {
             new Stop(0.71, Color.web("hotpink")),
             new Stop(1, Color.web("maroon")),});
 
-        Rectangle colors = new Rectangle(400, 400, grad);//safety orange
-        //colors.widthProperty().bind(scene.widthProperty());
-        //colors.heightProperty().bind(scene.heightProperty());
+        Rectangle colors = new Rectangle(1600, 1200, grad);
 
-        /*root.getChildren().add(colors);
-         root.getChildren().add(circles);*/
         Group blendModeGroup
-                = new Group(new Group(new Rectangle(400, 400,
-                                        Color.BLACK), circles), colors);
+                = new Group(new Group(new Rectangle(1600, 1200,
+                                        Color.BLACK), group), colors);
         colors.setBlendMode(BlendMode.OVERLAY);
-        group.getChildren().add(blendModeGroup);
-        circles.setEffect(new BoxBlur(10, 10, 3));
-
-        Timeline timeline = new Timeline();
-        for (Node circle : circles.getChildren()) {
-            timeline.getKeyFrames().addAll(
-                    new KeyFrame(Duration.ZERO, // set start position at 0
-                            new KeyValue(circle.translateXProperty(), random() * 400),
-                            new KeyValue(circle.translateYProperty(), random() * 400)
-                    ),
-                    new KeyFrame(new Duration(2000), // set end position at 40s
-                            new KeyValue(circle.translateXProperty(), random() * 400),
-                            new KeyValue(circle.translateYProperty(), random() * 400)
-                    )
-            );
-        }
-        
-        
-        EventHandler onFinished = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent t) {
-                 root.getChildren().remove(circles);
-                 animation(root);
-            }
-        };
-        
-        KeyFrame keyFrame = new KeyFrame(new Duration(2000), onFinished);
- 
-        //add the keyframe to the timeline
-        timeline.getKeyFrames().add(keyFrame);
-        
-        // play 40s of animation
-        timeline.play();
-
+        root.getChildren().add(blendModeGroup);
+        group.setEffect(new BoxBlur(3, 3, 3));
     }
+
 }
